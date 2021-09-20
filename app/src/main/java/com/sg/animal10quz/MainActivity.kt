3cab4175f281d177f3fea6b49a75668e28a184d0
shlomo <shlomo.gevim@gmail.com>
2021-09-20 22:29:19 +0300

    20/9/2021 before enter options file

--- app/src/main/java/com/sg/animal10quz/MainActivity.kt
+++ app/src/main/java/com/sg/animal10quz/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.sg.animal10quz
 
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Typeface
 import android.os.Bundle
+import android.preference.PreferenceManager
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -12,6 +16,18 @@ import android.view.MenuItem
 import com.sg.animal10quz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val GUESSES = "settings_numberOfGuesses"
+    val ANIMALS_TYPE = "settings_animalsType"
+    val QUIZ_BACKGROUND_COLOR = "settings_quiz_background_color"
+    val QUIZ_FONT = "settings_quiz_font"
+
+    private var isSettingsChanged = false
+
+
+    var chunkfive: Typeface? = null
+    var fontlerybrown: Typeface? = null
+    var wonderbarDemo: Typeface? = null
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -28,6 +44,14 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
+        chunkfive = Typeface.createFromAsset(assets, "fonts/Chunkfive.otf")
+        fontlerybrown = Typeface.createFromAsset(assets, "fonts/FontleroyBrown.ttf")
+        wonderbarDemo = Typeface.createFromAsset(assets, "fonts/Wonderbar Demo.otf")
+
+        PreferenceManager.setDefaultValues(this@MainActivity, R.xml.quiz_preferences, false)
+        PreferenceManager.getDefaultSharedPreferences(this@MainActivity)
+            .registerOnSharedPreferenceChangeListener(settingsChangeListener)
+
 
     }
 
@@ -38,13 +62,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
+
+       /* return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
-        }
+        }*/
+        val preferencesIntent = Intent(this@MainActivity, SettingActivity::class.java)
+        startActivity(preferencesIntent)
+        return super.onOptionsItemSelected(item)
+
+
+
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -52,4 +81,34 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+    private val settingsChangeListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            isSettingsChanged = true
+            /*if (key == GUESSES) {
+                myAnimalQuizFragment.modifyAnimalsGuessRows(sharedPreferences)
+                myAnimalQuizFragment.resetAnimalQuiz()
+            } else if (key == ANIMALS_TYPE) {
+                val animalTypes = sharedPreferences.getStringSet(MainActivity.ANIMALS_TYPE, null)
+                if (animalTypes != null && animalTypes.size > 0) {
+                    myAnimalQuizFragment.modifyTypeOfAnimalsInQuiz(sharedPreferences)
+                    myAnimalQuizFragment.resetAnimalQuiz()
+                } else {
+                    val editor = sharedPreferences.edit()
+                    animalTypes!!.add(getString(R.string.default_animal_type))
+                    editor.putStringSet(ANIMALS_TYPE, animalTypes)
+                    editor.apply()
+                    Toast.makeText(
+                        this@MainActivity,
+                        R.string.toast_message, Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } else if (key == QUIZ_FONT) {
+                myAnimalQuizFragment.modifyQuizFont(sharedPreferences)
+                myAnimalQuizFragment.resetAnimalQuiz()
+            } else if (key == QUIZ_BACKGROUND_COLOR) {
+                myAnimalQuizFragment.modifyBackgroundColor(sharedPreferences)
+                myAnimalQuizFragment.resetAnimalQuiz()
+            }
+            Toast.makeText(this@MainActivity, R.string.change_message, Toast.LENGTH_SHORT).show()*/
+        }
 }
